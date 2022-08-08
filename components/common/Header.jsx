ec8b115613ec3a17a4db7299be0a6784f8fed61a
Your Name <you@example.com>
2022-08-08 19:18:06 +0430

    header style

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -8,15 +8,15 @@ const Header = () => {
                 <Link href='/' passHref>
                     <a className='web-header'>
                         <div className="row web-header mb-3">
-                            <div className="col-sm-2 text-right">
+                            <div className="col-sm-2 text-right d-flex flex-column justify-content-end">
                                 <img src="images/lights.png" className="img-fluid d-none d-lg-inline" alt="traffic-lights" style={{width: "34%"}}/>
                             </div>
-                            <div className="col-sm p-2 p-lg-0 align-self-center">
+                            <div className="col-md p-2 p-lg-0 align-self-center">
                                 <p className="header-title text-center m-0 pt-1 pb-2">
                                     آزمون آیین نامه راهنمایی و رانندگی
                                 </p>
                             </div>
-                            <div className="col-sm-2 text-left">
+                            <div className="col-sm-2 text-left d-flex flex-column justify-content-end align-items-end">
                                 <img src="images/Warning.png" className="img-fluid d-none d-lg-inline" alt="warning-sign" style={{width: "45%"}}/>
                             </div>
                         </div>
